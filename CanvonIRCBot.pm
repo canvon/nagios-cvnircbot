0fee5aff4252eeab5e294ffe154e8e48ea7c7282
Fabian Pietsch <fabian@canvon.de>
2015-12-11 18:07:48 +0100

    Add debug output
    Otherwise, it just does what it does (not much, yet :-S), and prints
    mostly nothing. (It does seem to print the channels it's trying
    to join, though...) Now, if in debug mode, it logs what sub init
    is doing.
    
    Debug mode can be enabled by passing debug => 1 in the config file.

--- CanvonIRCBot.pm
+++ CanvonIRCBot.pm
@@ -9,6 +9,25 @@ use warnings;
 use FileHandle;
 use Fcntl qw(:DEFAULT :seek);
 
+# Get or set debugging state of the bot.
+sub debug
+{
+    my $bot = shift(@_);
+
+    $bot->{debug} = shift(@_) if (@_);
+
+    my $debug = 0;
+    $debug = $bot->{debug} if exists($bot->{debug});
+    return $debug;
+}
+
+sub log_debug
+{
+    my $bot = shift(@_);
+
+    $bot->log(@_) if $bot->debug();
+}
+
 sub get_nagios_logfile
 {
     my ($bot) = @_;
@@ -24,18 +43,22 @@ sub init
 
     my $nagios_logfile = $bot->get_nagios_logfile();
 
+    $bot->log_debug("Opening Nagios log file: $nagios_logfile");
     open(my $fh, '<', $nagios_logfile) or die("Can't open Nagios log file $nagios_logfile: $!\n");
 
     # Put the file handle into the bot state.
     $bot->{nagios_logfile_fh} = $fh;
 
     # Seek to EOF, for later waiting for more log lines to appear.
-    $fh->seek(0, SEEK_END);
+    $bot->log_debug("Seeking to end of Nagios log file...");
+    $fh->seek(0, SEEK_END) or die("Can't seek to end of Nagios log file: $!\n");
 
     # Set non-blocking IO.
+    $bot->log_debug("Setting Nagios log file file handle to non-blocking IO...");
     $fh->blocking(0) or die("Can't set Nagios log file file handle to non-blocking IO: $!\n");
 
     # Everything went well so far.
+    $bot->log_debug("Init succeeded.");
     return 1;
 }
 
