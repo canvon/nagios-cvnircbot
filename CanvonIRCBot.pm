ec4b5d91b0323d0e4464f2fb7de284bd23f01eb7
Fabian Pietsch <fabian@canvon.de>
2015-12-11 22:35:52 +0100

    Add parsing of real Nagios log lines; pass host/service notifications on to IRC

--- CanvonIRCBot.pm
+++ CanvonIRCBot.pm
@@ -67,6 +67,102 @@ sub init
     return 1;
 }
 
+sub parse_nagios_log_line
+{
+    my ($bot, $line) = @_;
+    my $ret = undef;
+
+    unless ($line =~ /^\[([0-9]+)\] (.*)$/)
+    {
+        $bot->log("Couldn't parse Nagios log line into time stamp and rest.");
+        return $ret;
+    }
+
+    my ($timestamp, $rest) = ($1, $2);
+    $ret = {};
+    $ret->{timestamp} = $timestamp;
+    $ret->{full_msg} = $rest;
+    $ret->{is_data} = 0;
+
+    unless ($rest =~ /^([^:]*): (.*)$/)
+    {
+        $bot->log_debug("Non-data Nagios message ("
+                       .localtime($ret->{timestamp})
+                       ."): ".$ret->{full_msg});
+        return $ret;
+    }
+
+    my ($type, $raw_data) = ($1, $2);
+    $ret->{is_data} = 1;
+    $ret->{type} = $type;
+    $ret->{type_recognized} = 0;
+    $ret->{raw_data} = $raw_data;
+
+    my @all_data = split(/;/, $raw_data);
+    $ret->{all_data} = [];
+    @{$ret->{all_data}} = @all_data;
+
+    if ($ret->{type} eq 'HOST ALERT')
+    {
+        $ret->{data} = {};
+        $ret->{data}{HOSTNAME}         = $all_data[0];
+        $ret->{data}{HOSTSTATE}        = $all_data[1];
+        $ret->{data}{HOSTSTATETYPE}    = $all_data[2];
+        $ret->{data}{HOSTATTEMPT}      = $all_data[3];
+        $ret->{data}{HOSTOUTPUT}       = $all_data[4];
+        $ret->{type_recognized} = 1
+    }
+    elsif ($ret->{type} eq 'SERVICE ALERT')
+    {
+        $ret->{data} = {};
+        $ret->{data}{HOSTNAME}         = $all_data[0];
+        $ret->{data}{SERVICEDESC}      = $all_data[1];
+        $ret->{data}{SERVICESTATE}     = $all_data[2];
+        $ret->{data}{SERVICESTATETYPE} = $all_data[3];
+        $ret->{data}{SERVICEATTEMPT}   = $all_data[4];
+        $ret->{data}{SERVICEOUTPUT}    = $all_data[5];
+        $ret->{type_recognized}        = 1
+    }
+    elsif ($ret->{type} eq 'HOST NOTIFICATION')
+    {
+        $ret->{data} = {};
+        $ret->{data}{CONTACTNAME}      = $all_data[0];
+        $ret->{data}{HOSTNAME}         = $all_data[1];
+        $ret->{data}{HOSTSTATE}        = $all_data[2];
+        $ret->{data}{notify_command}   = $all_data[3];
+        $ret->{data}{HOSTOUTPUT}       = $all_data[4];
+        $ret->{type_recognized} = 1
+    }
+    elsif ($ret->{type} eq 'SERVICE NOTIFICATION')
+    {
+        $ret->{data} = {};
+        $ret->{data}{CONTACTNAME}      = $all_data[0];
+        $ret->{data}{HOSTNAME}         = $all_data[1];
+        $ret->{data}{SERVICEDESC}      = $all_data[1];
+        $ret->{data}{SERVICESTATE}     = $all_data[2];
+        $ret->{data}{notify_command}   = $all_data[3];
+        $ret->{data}{SERVICEOUTPUT}    = $all_data[4];
+        $ret->{type_recognized} = 1
+    }
+
+    unless ($ret->{type_recognized})
+    {
+        $bot->log_debug("Unrecognized data Nagios message ("
+                       .localtime($ret->{timestamp})
+                       ."): type=\"".$ret->{type}."\", raw data: "
+                       .$ret->{raw_data});
+    }
+    #else
+    #{
+    #    $bot->log_debug("Recognized data Nagios message ("
+    #                   .localtime($ret->{timestamp})
+    #                   ."): type=\"".$ret->{type}."\", data hash entries: "
+    #                   .scalar(keys %{$ret->{data}}));
+    #}
+
+    return $ret;
+}
+
 sub tick
 {
     my ($bot) = @_;
@@ -86,14 +182,50 @@ sub tick
     {
         chomp($line);
 
+        my $msg = $bot->parse_nagios_log_line($line);
+        unless (defined($msg))
+        {
+            $bot->log("Couldn't parse Nagios log line: $line");
+            return $next_tick_secs;
+        }
+
         # Got a log line, pass it on to the configured channels.
         $bot->log_debug("Got a Nagios log message, passing it on to configured channels...");
         foreach my $channel (@{$bot->{nagios_channels}})
         {
-            $bot->log_debug("Passing to $channel: $line");
+            my $out = '';
+            if ($msg->{is_data} && $msg->{type_recognized})
+            {
+                if ($msg->{type} eq 'HOST NOTIFICATION')
+                {
+                    $out .= $msg->{data}{CONTACTNAME}.": ** Host Alert: " .
+                            $msg->{data}{HOSTNAME}." is ".
+                            $msg->{data}{HOSTSTATE}." **  ".
+                            "Info: ".$msg->{data}{HOSTOUTPUT}."  ".
+                            "Date/Time: ".localtime($msg->{timestamp});
+                }
+                elsif ($msg->{type} eq 'SERVICE NOTIFICATION')
+                {
+                    $out .= $msg->{data}{CONTACTNAME}.": ** Service Alert: " .
+                            $msg->{data}{HOSTNAME}."/".
+                            $msg->{data}{SERVICEDESC}." is ".
+                            $msg->{data}{SERVICESTATE}." **  ".
+                            "Info: ".$msg->{data}{SERVICEOUTPUT}."  ".
+                            "Date/Time: ".localtime($msg->{timestamp});
+                }
+            }
+            else
+            {
+                $bot->log_debug("Not passing unknown/unwanted message on: $line");
+            }
 
-            # For now, simply send the whole line, unchecked and unformatted.
-            $bot->say(channel => $channel, body => $line);
+            # Send the constructed line to the channel.
+            # (But only if it has in fact been constructed.)
+            if (length($out) >= 1)
+            {
+                $bot->log_debug("Passing to $channel: $out");
+                $bot->say(channel => $channel, body => $out);
+            }
         }
     }
 
