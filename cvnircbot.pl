80cebdeabfd5d17224f7daa2489ab913dc19612b
Fabian Pietsch <fabian@canvon.de>
2019-02-02 15:34:44 +0100

    Add command-line option parsing; add --config-file=CONFFILE

--- cvnircbot.pl
+++ cvnircbot.pl
@@ -10,6 +10,27 @@ my $config;
 
 (my $basename = $0) =~ s#/.*/##;
 
+while (scalar(@ARGV) > 0) {
+    my $arg = shift;
+    if ($arg !~ /^-/) {
+        unshift(@ARGV, $arg);
+        last;
+    }
+
+    if ($arg =~ /^(-\?|-h|--help|--usage)$/) {
+        print("Usage: $basename [--config-file=CONFFILE]\n");
+        exit(0);
+    }
+    elsif ($arg =~ /^--config-file=(.*)$/) {
+        $conffile = $1;
+    }
+    else {
+        die("$basename: Invalid argument \"$arg\"\n");
+    }
+}
+
+scalar(@ARGV) == 0 or die("$basename: Invalid arguments\n");
+
 unless ($config = do $conffile) {
     die "$basename: couldn't parse $conffile: $@\n" if $@;
     die "$basename: couldn't do $conffile: $!\n"    unless defined $config;
